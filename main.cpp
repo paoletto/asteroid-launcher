416172c414947a59cf2604488447e34fc117f9e3
Paolo <paoletto@gmail.com>
2021-12-17 22:33:23 +0200

    remove AA HighDpiScaling

--- main.cpp
+++ main.cpp
@@ -96,7 +96,6 @@ public:
 
 int main(int argc, char **argv)
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QmlPath::append(":/qml/");
     HomeApplication app(argc, argv, QString());
 
